66a422a5e2806e046b54f4c6e380bd5335e0a2fb
wahabxvii <auwa.mm@gmail.com>
2026-02-23 18:34:29 +0300

    Add: added comment

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * print a string
+ * Return: 0
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 }
