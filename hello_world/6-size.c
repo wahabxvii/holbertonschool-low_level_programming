da61d9ebe5bf4effe483eebf984493e3da8ab2e0
wahabxvii <auwa.mm@gmail.com>
2026-02-24 11:30:31 +0300

    Fix: added missing (byte(s))

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int num = 9;
 int lnum = 19;
 int llnum = 119;
 float flot = 1.9;
-printf("Size of a char: %zu\n", sizeof(let));
-printf("Size of an int: %zu\n", sizeof(num));
-printf("Size of a long int: %zu\n", sizeof(lnum));
-printf("Size of a long long int: %zu\n", sizeof(llnum));
-printf("Size of a float: %zu\n", sizeof(flot));
+printf("Size of a char: %zu byte(s)\n", sizeof(let));
+printf("Size of an int: %zu byte(s)\n", sizeof(num));
+printf("Size of a long int: %zu byte(s)\n", sizeof(lnum));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(llnum));
+printf("Size of a float: %zu byte(s)\n", sizeof(flot));
 return (0);
 }
