0058f9d1e72f11be8bb2f1ace75b4bb45c16c9d9
wahabxvii <auwa.mm@gmail.com>
2026-02-24 11:38:35 +0300

    well it a long story

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,15 +6,10 @@
  */
 int main(void)
 {
-char let = 'C';
-int num = 9;
-int lnum = 19;
-int llnum = 119;
-float flot = 1.9;
-printf("Size of a char: %zu byte(s)\n", sizeof(let));
-printf("Size of an int: %zu byte(s)\n", sizeof(num));
-printf("Size of a long int: %zu byte(s)\n", sizeof(lnum));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(llnum));
-printf("Size of a float: %zu byte(s)\n", sizeof(flot));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
