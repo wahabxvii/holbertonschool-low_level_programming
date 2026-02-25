f28caa4721b5175a23c0caf43aa413123d6f2a4f
wahabxvii <auwa.mm@gmail.com>
2026-02-25 17:22:15 +0300

    Fix space after if

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ if (lasdig > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lasdig);
 }
-else if(lasdig == 0)
+else if (lasdig == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lasdig);
 }
-else if(lasdig < 6 && lasdig != 0)
+else if (lasdig < 6 && lasdig != 0)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lasdig);
 }
