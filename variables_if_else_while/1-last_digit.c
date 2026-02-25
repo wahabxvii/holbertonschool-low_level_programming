a4d9bc3dab814979322c40ea3e72edc4b07973ea
wahabxvii <auwa.mm@gmail.com>
2026-02-25 17:14:27 +0300

    Fix typo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,18 +9,18 @@
 int main(void)
 {
 int n;
+int lasdig = n % 10;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int lasdig = n % 10;
 if (lasdig > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, lasdig);
 }
-if else(lasdig == 0)
+else if(lasdig == 0)
 {
-pritf("Last digit of %d is %d and is 0\n", n, lasdig);
+printf("Last digit of %d is %d and is 0\n", n, lasdig);
 }
-if else(lasdig < 6 && lasdig != 0)
+else if(lasdig < 6 && lasdig != 0)
 {
 printf("Last digit of %d is %d and less than 6 and not 0\n", n, lasdig);
 }
