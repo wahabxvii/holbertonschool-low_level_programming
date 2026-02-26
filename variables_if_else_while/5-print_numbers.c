17624d3ec3fa536528874e10f4e8a19dd06ba3d5
wahabxvii <auwa.mm@gmail.com>
2026-02-26 11:20:22 +0300

    Create 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * print numbers of base 10
+ * Return: zero
+ */
+int main(void)
+{
+int n;
+for (n = 1;
+n <=9 ; n++)
+{
+printf("%d", n);
+}
+putchar('\n');
+return (0);
+}
