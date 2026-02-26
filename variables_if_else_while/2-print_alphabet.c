b83440df8d53819d9d78809610ec275792c84d3d
wahabxvii <auwa.mm@gmail.com>
2026-02-26 10:14:43 +0300

    Create 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ * print lowercase alphabet
+ * Return: zero
+ */
+
+int main(void)
+{
+int c;
+for
+(c = 97;
+c <= 122; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
