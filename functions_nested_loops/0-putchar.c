c3e99303d147a5148c56ed624b393e8994b6ac1f
wahabxvii <auwa.mm@gmail.com>
2026-02-27 18:44:49 +0300

    Create: 0 file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
-#include <unistd.h>
+#include "main.h"
 /**
  * main -starting point
  * print _putchar without standered liprarys
  * Return: 0
  */
-int _putchar(char c);
 int main(void)
 {
 const char *str = "_putchar";
 int n = 0;
 while (str[n] != '\0')
 {
-_putchar(n);
+_putchar(str[n]);
 n++;
 }
-_putchar("\n");
+_putchar('\n');
 return (0);
 }
