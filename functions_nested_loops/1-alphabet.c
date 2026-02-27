9bb3931602a03434370ea57a9d24207fbb161102
wahabxvii <auwa.mm@gmail.com>
2026-02-27 22:34:33 +0300

    Create: file

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ * print alphabet lower case
+ * without useing standered librarys
+ * Return: Always 0.
+ */
+int main(void);
+{
+int c;
+for (c = 'a';
+c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
