d583080615810fa4ff18ef7f1c56a9ead4fb5b04
wahabxvii <auwa.mm@gmail.com>
2026-02-28 10:31:40 +0300

    Create: file 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - chechs if the letter is lower case
+ * Return: 1 if c lowercase
+ * Return: 0 otherwise
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return(1);
+}
+else
+{
+return(0);
+}
+_putchar('\n');
+}
