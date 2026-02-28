2bdaec7a2cd4ed34635a50df3c5530586c8f74a3
wahabxvii <auwa.mm@gmail.com>
2026-02-28 21:09:44 +0300

    edit the function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,22 +10,20 @@ for (r = 0; r <= 9; r++)
 {
 for (c = 0; c <= 9; c++)
 {
-p = r * c;            
+p = r * c;
 if (c != 0)
 {
 _putchar(',');
-}         
+_putchar(' ');
+}
 if (p < 10)
 {
-_putchar(' ');
 _putchar(' ');
 }
 else
 {
-_putchar(' ');
-}
-if (p >= 10)
 _putchar((p / 10) + '0');
+}
 _putchar((p % 10) + '0');
 }
 _putchar('\n');
