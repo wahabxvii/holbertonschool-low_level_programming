154d40552c9747407930dc5767b381b6bbbea0d2
wahabxvii <auwa.mm@gmail.com>
2026-02-28 13:31:53 +0300

    Create: file 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *@v - character in ASCII
+ * Return: absolute value
+ */
+int _abs(int v)
+{
+if (v >= 0)
+{
+return (v);
+}
+else
+{
+return (v * -1);
+}
+}
