52b2037d9cc75f35911c8076e5273bb6f67a5957
wahabxvii <auwa.mm@gmail.com>
2026-02-28 12:02:37 +0300

    Create: file 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of the number
+ * Return: 1 if +, 0 if 0 or -1 if -
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+'','' ''\n', n);
+}
+else if (n = 0)
+{
+_putchar('0'','' ''\n', n);
+}
+else
+{
+_putchar('-'','' ''\n', n)
+}
+_putchar('\n')
+}
