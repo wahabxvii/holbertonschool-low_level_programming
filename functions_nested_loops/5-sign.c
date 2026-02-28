75fdddb1c22ce232396e2de95972a08def729051
wahabxvii <auwa.mm@gmail.com>
2026-02-28 12:07:41 +0300

    change

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,15 +7,18 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putchar('+'','' ''\n', n);
+_putchar('+');
+return (1);
 }
 else if (n = 0)
 {
-_putchar('0'','' ''\n', n);
+_putchar('0');
+return (0);
 }
 else
 {
-_putchar('-'','' ''\n', n)
+_putchar('-');
+return (1);
 }
 _putchar('\n')
 }
