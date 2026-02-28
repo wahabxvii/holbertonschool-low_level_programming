e1dc44740d27be4e71c5e0cd5f9acb438d0939e9
wahabxvii <auwa.mm@gmail.com>
2026-02-28 15:02:09 +0300

    Add discrption for n

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a number
- * @
+ * @n: integer number
  * Return: the value of the last digit
  */
 int print_last_digit(int n)
