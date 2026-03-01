bc563d64cb898294181830db22a7b399198668ec
wahabxvii <auwa.mm@gmail.com>
2026-03-01 16:45:34 +0300

    fix

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,12 +6,13 @@
 void print_most_numbers(void)
 {
 	int i;
-	if (i != 2 && i != 4)
-	{	
-		for (i = 0;
-			i <= 9; i++)
+
+	for (i = 0;
+		i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
 		{
-			_putchar(i);
+			_putchar(i + '0');
 		}
 	}
 	_putchar('\n');
