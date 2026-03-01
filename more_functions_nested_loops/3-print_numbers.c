5e186e15c76f4864dc7e644d021968bb06f276d1
wahabxvii <auwa.mm@gmail.com>
2026-03-01 15:54:56 +0300

    Create: file 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9
+ * Return: nothing
+ */
+int print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
