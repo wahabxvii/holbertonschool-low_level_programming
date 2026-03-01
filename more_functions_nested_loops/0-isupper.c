f26d7f653f62710e7d358fd31a372d565e92b070
wahabxvii <auwa.mm@gmail.com>
2026-03-01 11:32:48 +0300

    Create: file 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * @c: charcter in ASCII
+ * Return: 1 if upper or 0 if not
+ */
+int _isupper(int c)
+{
+	if (c > 64;
+	c < 91; c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
