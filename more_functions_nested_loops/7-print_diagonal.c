5920c17be1128ba2d207b7ef51051d17c1c05faa
wahabxvii <auwa.mm@gmail.com>
2026-03-01 21:37:07 +0300

    Create: file 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: variable
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(' ');
+			_putchar('\\');
+		}
+	_putchar(' ');
+	}
+}
