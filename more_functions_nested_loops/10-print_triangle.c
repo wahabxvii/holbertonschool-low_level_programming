f63f84b1f45cdfef00fb8ba907f352b894d27dea
wahabxvii <auwa.mm@gmail.com>
2026-03-02 14:22:27 +0300

    Create: file 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle -
+ * @size: variable
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= size; i++){
+		for (j = 1; j <= size - i; j++){
+			_putchar('\n');
+		}
+		for (i = 1; i <= size; i++){
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+}
