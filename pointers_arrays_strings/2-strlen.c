1ff2b0e90acad79b78acddef779f302c73848b5e
wahabxvii <auwa.mm@gmail.com>
2026-03-03 16:01:15 +0300

    Create: file 1

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - print the length of string
+ * @s: variable pointer
+ * Return:
+ */
+int _strlen(int *s)
+{
+	while (s != '\0')
+	{
+		_putchar(s);
+		_putchar('\n');
+	}
+	return (0);
+}
