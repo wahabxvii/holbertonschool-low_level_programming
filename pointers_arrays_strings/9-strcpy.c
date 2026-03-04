c9ce7427074444823f4323b4842dcade79dd716f
wahabxvii <auwa.mm@gmail.com>
2026-03-04 16:08:11 +0300

    Create: file 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy -
+ *
+ *
+ * Return:
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (ptr);
+}
