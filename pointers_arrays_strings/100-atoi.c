a466a234aee318821feeb30bf1973c90fe4f8ff6
wahabxvii <auwa.mm@gmail.com>
2026-03-04 21:56:39 +0300

    Create file 100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - convert a string to an integer
+ * @s: pointer to a string
+ * Return: int number
+ */
+int _atoi(char *s)
+{
+	short parity = 0;
+	int num = 0;
+
+	while (*s == '+' || *s == '-')
+	{
+		if (*s == '-')
+			parity++;
+		++s;
+	}
+	while (*s >= 48 && *s <= 57)
+	{
+		num *= 10;
+		num += *s - 48;
+		++s;
+	}
+	if (!(parity % 2))
+		return (-num);
+	return (num);
+}
