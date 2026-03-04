0c6d9724678547f7ffe3da0ecd9c46ea3af376b2
wahabxvii <auwa.mm@gmail.com>
2026-03-04 14:19:32 +0300

    Create: file 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: pointer to a string
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+	int n;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len + 1) / 2;
+	}
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+}
