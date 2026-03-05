d37f3e20d84bcb3f42729a36f4c3bb369a9cc26d
wahabxvii <auwa.mm@gmail.com>
2026-03-05 13:51:27 +0300

    Fix: overfow

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,39 @@
-#include "main.h"
-/**
- * _atoi - convert a string to an integer
- * @s: pointer to a string
- * Return: int number
- */
 int _atoi(char *s)
 {
 	int sign = 1;
 	int num = 0;
 	int digit_found = 0;
 
-	while (*s != '\0')
+	while (*s)
 	{
 		if ((*s == '+' || *s == '-') && !digit_found)
 		{
 			if (*s == '-')
-				sign *= -1;
+				sign = -sign;
 			s++;
 		}
-		else if (*s >= 48 && *s <= 57)
+		else if (*s >= '0' && *s <= '9')
 		{
+			int digit = *s - '0';
 			digit_found = 1;
-			num *= 10;
-			num += *s - 48;
+			if (sign == 1)
+			{
+				if (num > (2147483647 - digit) / 10)
+				{
+					num = 2147483647;
+					while (*(++s) >= '0' && *s <= '9')
+						;
+					return num;
+				}
+			}
+			else
+			{
+				if (num > (-(2147483647 + digit)) / 10)
+				{
+					return 2147483647;
+				}
+			}
+			num = num * 10 + digit;
 			s++;
 		}
 		else if (digit_found)
@@ -30,9 +41,11 @@ int _atoi(char *s)
 			break;
 		}
 		else
-		{
 			s++;
-		}
 	}
-	return (num * sign);
+	if (!digit_found)
+		return 0;
+	if (num == 2147483647 && sign == -1)
+		return 2147483647;
+	return sign * num;
 }
