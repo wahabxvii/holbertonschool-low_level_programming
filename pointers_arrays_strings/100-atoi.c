d53e038a6d781b4024928b41d276daca1b1f143f
wahabxvii <auwa.mm@gmail.com>
2026-03-05 14:01:52 +0300

    Fix: overfow

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,52 +1,38 @@
- #include "main.h"
+#include "main.h"
 /**
-* _atoi - convert a string to an integer
-* @s: pointer to a string
-* Return: int number
-*/
-#define INT_MAX_VAL ((~0U) >> 1)
-#define INT_MIN_VAL (-(INT_MAX_VAL) - 1)
-
+ * _atoi - convert a string to an integer
+ * @s: pointer to a string
+ * Return: int number
+ */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int num = 0;
-    int digit_found = 0;
+	int sign = 1;
+	int num = 0;
+	int digit_found = 0;
 
-    while (*s)
-    {
-        if ((*s == '+' || *s == '-') && !digit_found)
-        {
-            if (*s == '-')
-                sign = -sign;
-            s++;
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            int digit = *s - '0';
-            digit_found = 1;
-            if (num > (INT_MAX_VAL - digit) / 10)
-            {
-                while (*(++s) >= '0' && *s <= '9')
-                    ;
-                return (sign == 1) ? INT_MAX_VAL : INT_MIN_VAL;
-            }
-
-            num = num * 10 + digit;
-            s++;
-        }
-        else if (digit_found)
-        {
-            break;
-        }
-        else
-        {
-            s++;
-        }
-    }
-
-    if (!digit_found)
-        return 0;
-
-    return sign * num;
+	while (*s != '\0')
+	{
+		if ((*s == '+' || *s == '-') && !digit_found)
+		{
+			if (*s == '-')
+				sign *= -1;
+			s++;
+		}
+		else if (*s >= 48 && *s <= 57)
+		{
+			digit_found = 1;
+			num *= 10;
+			num += *s - 48;
+			s++;
+		}
+		else if (digit_found)
+		{
+			break;
+		}
+		else
+		{
+			s++;
+		}
+	}
+	return (num * sign);
 }
