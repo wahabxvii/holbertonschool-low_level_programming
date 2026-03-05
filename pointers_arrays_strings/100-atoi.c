2b79f5c5633565891e83a25e685068a72b1ff782
wahabxvii <auwa.mm@gmail.com>
2026-03-05 13:30:51 +0300

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,33 @@
 #include "main.h"
 /**
-* _atoi - convert a string to an integer
-* @s: pointer to a string
-* Return: int number
-*/
-int _atoi(char *s) {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+ * _atoi - convert a string to an integer
+ * @s: pointer to a string
+ * Return: int number
+ */
+int _atoi(char* str)
+{
+	int sign = 1, base = 0, i = 0;
 
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	if (s[0] == '+')
+	while (str[i] == ' ')
 	{
 		i++;
 	}
-	while (s[i] != '\0')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
+		sign = 1 - 2 * (str[i++] == '-');
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (base > 2147483647 / 10
+			|| (base == 2147483647 / 10
+			&& str[i] - '0' > 7))
 		{
-			result = result * 10 + (s[i] - '0');
+			if (sign == 1)
+				return 2147483647;
+			else
+				return 2147483647;
 		}
-		else
-		{
-			break;
-		}
-		i++;
+		base = 10 * base + (str[i++] - '0');
 	}
-	result = result * sign;
-	return result;
+	return base * sign;
 }
