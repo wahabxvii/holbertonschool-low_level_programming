eacb58fc2398b6c08686672ddf8895b4b802231a
wahabxvii <auwa.mm@gmail.com>
2026-03-06 17:09:33 +0300

    Fix

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -6,12 +6,13 @@
  */
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0;
-		s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
 		for (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
+		i++;
 	}
+	return (s);
 }
