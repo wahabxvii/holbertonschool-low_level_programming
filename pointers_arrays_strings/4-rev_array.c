b55063f9db8ab7bd64489e39fd9f099e9569a0f4
wahabxvii <auwa.mm@gmail.com>
2026-03-06 16:40:52 +0300

    use diffirent implimntation

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,15 @@
 void reverse_array(int *a, int n)
 {
 	int i = 0;
+	int j = n - 1;
+	int tmp;
 
-	while (i < n && a[i] != '\0')
-		i++;
-	while (a[i] > 0)
+	while (i < j)
 	{
-		_putchar(a[i]);
-		i--;
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		i++;
+		j--;
 	}
 }
