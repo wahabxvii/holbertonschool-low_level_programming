2e175148147206f46520625663e0d80cc74217a1
wahabxvii <auwa.mm@gmail.com>
2026-03-06 15:04:13 +0300

    Create file 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *
+ * Return:
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' || s2[i] == '\0')
+			break;
+		i++;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+		return (0);
+	else
+		return (s1[i] - s2[i]);
+}
