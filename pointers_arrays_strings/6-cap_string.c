93c08ff7b067d05cf1cc069adc7a1c3a9702f237
wahabxvii <auwa.mm@gmail.com>
2026-03-06 17:20:13 +0300

    Create: file 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * cap_string -
+ * @s: 
+ * Return:
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] = s[i] + 32;
+		i++;
+		else
+			continue;
+	}
+	return (s);
+}
