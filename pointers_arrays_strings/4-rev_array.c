963b02df8ddcf8cbe6ba0a1e876e273689c9fdfe
wahabxvii <auwa.mm@gmail.com>
2026-03-06 16:11:31 +0300

    Create: file 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - reverse array
+ * @a: pointer to an array
+ * @n: intger
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < n && a[i] != '\0')
+		i++;
+	while (a[i] >= 0)
+	{
+		_putchar(a[i]);
+		i--;
+	}
+}
