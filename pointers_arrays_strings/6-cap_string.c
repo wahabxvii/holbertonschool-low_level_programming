50060f05909f70a7c2cae1e0e8a26e79033c6fe5
wahabxvii <auwa.mm@gmail.com>
2026-03-07 13:17:37 +0300

    Remove debug lines

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,15 +16,3 @@ char *cap_string(char *s)
 	}
 	return (s);
 }
-
-#include <stdio.h>
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
-}
