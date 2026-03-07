226f30afc6708fe96c7b89a99581a70d29b8a376
wahabxvii <auwa.mm@gmail.com>
2026-03-07 13:22:25 +0300

    Fix

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,8 +10,8 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
-			s[i] = s[i] + 32;
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 		i++;
 	}
 	return (s);
