195814fa4974d55d8e3b029b494672761b435052
wahabxvii <auwa.mm@gmail.com>
2026-03-07 14:41:12 +0300

    fix

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,16 +2,36 @@
 /**
  * cap_string - capitalizes all words of a string
  * @s: pointer to a string
- * Return: a string
+ * Return: pointer to a string
  */
 char *cap_string(char *s)
 {
-	int i = 0;
+	int i , j;
+	const char sep[] = " \t\n,;.!?\"(){}";
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
-			s[i] = s[i] - 32;
+		{
+			if (i == 0)
+			{
+				s[i] = s[i] - ('a' - 'A');
+			}
+			else
+			{
+				j = 0;
+				while (sep[j] != '\0')
+				{
+					if (s[i] == sep[j])
+					{
+						s[i] = s[i] - ('a' - 'A');
+						break;
+					}
+					j++;
+				}
+			}
+		}
 		i++;
 	}
 	return (s);
