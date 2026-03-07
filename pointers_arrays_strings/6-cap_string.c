bb81a3008de1f48d5d0fffa1c18dffc0f7c44eba
wahabxvii <auwa.mm@gmail.com>
2026-03-07 13:16:31 +0300

    Fix

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,19 @@ char *cap_string(char *s)
 	{
 		if (s[i] >= 'A' && s[i] <= 'Z')
 			s[i] = s[i] + 32;
-		else
-			continue;
 		i++;
 	}
 	return (s);
+}
+
+#include <stdio.h>
+int main(void)
+{
+    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+    char *ptr;
+
+    ptr = cap_string(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
