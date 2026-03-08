31a3af4d32054de0106df1d658efd58d682ee13a
wahabxvii <auwa.mm@gmail.com>
2026-03-08 17:53:30 +0300

    Fix: Betty style

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* _memcpy - 
-* @
-* @
-* @
-* Return: 
+* _memcpy - copies memory area
+* @dest: pointer destination string
+* @src: pointer source string
+* @n: number of copied chars
+* Return: pointer to a string
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
