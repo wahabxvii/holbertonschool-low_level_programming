c301ab548ee74baf99936ef91a0fce44a63dcf1c
wahabxvii <auwa.mm@gmail.com>
2026-03-10 10:17:42 +0300

    add all files

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -39,4 +39,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 #endif
