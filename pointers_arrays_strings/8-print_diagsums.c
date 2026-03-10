4024214d70a49c123b47005308f65bf77cfcc0e1
wahabxvii <auwa.mm@gmail.com>
2026-03-10 14:24:19 +0300

    Create file 8-print_chessboard

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/** 
+* @a:
+* @size:
+* Return: 
+*/
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0;
+	  i < size; i++)
+	{
+		sum1 += a[i * size + 1];
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
