3824f7730c207385848c88c029b43025e0a23b4c
wahabxvii <auwa.mm@gmail.com>
2026-03-10 10:52:27 +0300

    update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -41,4 +41,5 @@ char *string_toupper(char *);
 char *cap_string(char *);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #endif
