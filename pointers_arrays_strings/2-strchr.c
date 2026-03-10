5918be5ae6c8e6b242d7d27c0f585aaf3841e312
wahabxvii <auwa.mm@gmail.com>
2026-03-10 16:12:35 +0300

    Fix file 2-strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,8 +10,10 @@ char *_strchr(char *s, char c)
 	while (*s != '\0')
 	{
 		if (*s == c)
-			return ((char *) s);
+			return (s);
 		s++;
 	}
-	return (s);
+	if (*s == c)
+		return (s);
+	return (0);
 }
