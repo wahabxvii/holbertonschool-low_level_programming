b1c6ca4ab3e96c2c2a1e713768ce384ee66fd959
wahabxvii <auwa.mm@gmail.com>
2026-03-10 10:51:35 +0300

    Create file 5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* _strstr -
+* @
+* @ 
+* Return: 
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
+	}
+	return (0);
+}
