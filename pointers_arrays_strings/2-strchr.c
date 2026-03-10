a53e033761f231ea2e4c2b19288194d48b5425a4
wahabxvii <auwa.mm@gmail.com>
2026-03-10 15:24:46 +0300

    fix

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -15,25 +15,3 @@ char *_strchr(char *s, char c)
 	}
 	return (s);
 }
-
-
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *s = "hello";
-    char *f;
-
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
-}
