ceaa79520c50f6a0b65840f14290f4e92bbeeef6
wahabxvii <auwa.mm@gmail.com>
2026-03-11 14:51:27 +0300

    Create file 1-print_rev_recursion.c

--- recursion/main.h
+++ recursion/main.h
@@ -45,4 +45,6 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strchr(char *s, char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 #endif
