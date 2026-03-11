d107bd0dd69dcf383d4e75a0770b492e670e3282
wahabxvii <auwa.mm@gmail.com>
2026-03-11 16:26:36 +0300

    fix Betty style

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,12 +2,11 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: pointer to a string
-* Retrun: length of string
+ * Return: integer
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
-	else
-	 return (1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
