a04732a1b428c8ac0c80e54c8058fb0e17afa953
wahabxvii <auwa.mm@gmail.com>
2026-03-11 13:40:24 +0300

    Create file 0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _puts_recursion - prints a string
+* @s: pointer to a string
+* Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
