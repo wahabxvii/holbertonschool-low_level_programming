8229737d35d971fc57f96bf33607c8938b037c23
root <root@localhost.localdomain>
2026-03-11 16:34:25 +0000

    add discription

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* factorial - 
-* @n: 
-* Return:
+* factorial - returns the factorial of a given number
+* @n: given integer
+* Return: integer
 */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
-  if (n == 0 || n == 1)
-    return (1);
+	if (n == 0 || n == 1)
+		return (1);
 	return (n * factorial(n - 1));
 }
