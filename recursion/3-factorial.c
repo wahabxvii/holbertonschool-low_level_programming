820e76fadc3338a77636bb678134e04ac94ca5c2
wahabxvii <auwa.mm@gmail.com>
2026-03-11 18:47:53 +0300

    Fix add base

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
-*
-* @n:
+* factorial - 
+* @n: 
 * Return:
 */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
+  if (n == 0 || n == 1)
+    return (1);
 	return (n * factorial(n - 1));
 }
