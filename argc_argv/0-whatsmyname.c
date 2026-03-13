d67012480243a4e38c7f4d63ffb9a78e559b015b
wahabxvii <auwa.mm@gmail.com>
2026-03-13 22:41:12 +0300

    Fix Betty style

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
-*
-*
+* main - prints program name
+* @argc: argv count
+* @argv: givin arguments
+* Return: intger
 */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
