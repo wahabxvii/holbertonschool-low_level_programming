9e1238f60e110d2dc417efebde6a60c988933969
wahabxvii <auwa.mm@gmail.com>
2026-03-13 15:34:40 +0300

    Create file 6-is_prime_number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* is_prime_number - returns 1 if the input integer is a prime otherwise return 0
+* @n: givin number
+* Return: integer
+*/
+int helper(int n, int i);
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+	return (helper(n, 2));
+}
+/**
+* helper - fides if prime or not
+* @n: givin number
+* Return: integer
+*/
+int helper(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (helper(n, i +1));
+}
