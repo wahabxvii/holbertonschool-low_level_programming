67a2378b63e32834a057ced29c42d9e9dd8a1e13
wahabxvii <auwa.mm@gmail.com>
2026-03-14 11:59:48 +0300

    Create file 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - prints program name
+* @argc: argv count
+* @argv: givin arguments
+* Return: zero
+*/
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
