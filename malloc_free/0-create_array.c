9ff496d448e3064eb9bcf8e90882d00a3834e417
wahabxvii <auwa.mm@gmail.com>
2026-03-31 16:08:44 +0300

    fix if statment

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -11,7 +11,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array = malloc(size * sizeof(char));
 
-	if (size == 1)
+	if (size == 0)
 		return (0);
 	if (array == 0)
 		return (0);
