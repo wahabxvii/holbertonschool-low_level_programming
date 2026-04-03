fe128cf6302c0a7d13476a1e9646f6f0d66202fd
wahabxvii <auwa.mm@gmail.com>
2026-04-03 15:01:45 +0300

    Create file 4-free_grid.c

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -6,4 +6,6 @@ char *_strdup(char *str);
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int hight);
+void free_grid(int **grid, int height);
 #endif
