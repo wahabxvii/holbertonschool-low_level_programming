557ab18b8b0a0c0cd00a9e5a684e26c861eb1e79
wahabxvii <auwa.mm@gmail.com>
2026-04-04 10:26:44 +0300

    Create file 1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* string_nconcat - concatenates two strings.
+* @s1: frist givin string
+* @s2: second givin string
+* @n: number of second string characters taken
+*
+* Return: pointer to a string.
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *nstr;
+	unsigned int s1len;
+	unsigned int i;
+
+	s1len = _strlen(s1);
+	nstr = malloc(s1len + n * sizeof(char));
+	for (i = 0;
+	i < s1len; i++)
+		nstr[i] = s1[i];
+	for (i = 0;
+	  i < n; i++)
+		nstr[i + s1len] = s2[i];
+	return (nstr);
+}
+/**
+ * _strlen - print the length of string
+ * @s: variable pointer
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -9,4 +9,5 @@ char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int hight);
 void free_grid(int **grid, int height);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
