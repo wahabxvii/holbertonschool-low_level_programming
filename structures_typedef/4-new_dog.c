4d4eb1b8483ba2b577e33523ed2e5f72ce37b48c
wahabxvii <auwa.mm@gmail.com>
2026-04-07 19:29:19 +0300

    fix free everyting if malloc fails

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -29,6 +29,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(_strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
