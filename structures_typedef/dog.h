045a39db44c30da455e0262c35e63ae7324cc128
wahabxvii <auwa.mm@gmail.com>
2026-04-07 12:34:54 +0300

    add discription

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+* struct dog - save dog info
+* @name: dog name
+* @age: dog age
+* @owner: stupid dog owner :)
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
