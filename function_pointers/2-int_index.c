ad439359bbcf8acc1e1fb97ccfc52a4e0cf8a1b8
wahabxvii <auwa.mm@gmail.com>
2026-04-09 17:59:59 +0300

    fix if array is NULL

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
 * int_index - searches for an integer.
 * @array: pointer to an array of ints
@@ -11,6 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (array == NULL)
+		return (-1);
 	if (size <= 0)
 		return (-1);
 	for (i = 0;
