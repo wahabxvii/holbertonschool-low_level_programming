5e63d369018d75d9251a719f8a12e70fc6c341b3
wahabxvii <auwa.mm@gmail.com>
2026-04-09 14:06:06 +0300

    fix Betty style

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,12 @@
 #include "function_pointers.h"
 /**
+* print_name - prints a name.
+* @name: pointer to a string
+* @f: pointer to a function
 *
+* Return: nothing.
 */
 void print_name(char *name, void (*f)(char *))
 {
- 	f(name);
+	f(name);
 }
