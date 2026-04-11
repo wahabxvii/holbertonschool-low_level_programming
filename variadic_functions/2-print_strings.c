eb6ebcde0686c91c82657b28b9a3ca1b4d2bc06f
wahabxvii <auwa.mm@gmail.com>
2026-04-11 14:28:22 +0300

    fix add () around nil

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			x = va_arg(args, char*);
 			if (x == 0)
-				printf("nil");
+				printf("(nil)");
 			else
 			{
 				printf("%s", x);
