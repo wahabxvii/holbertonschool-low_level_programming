022352c10ec04f65559e403f699f157eed63c763
wahabxvii <auwa.mm@gmail.com>
2026-04-11 20:27:42 +0300

    use diffrent emplemtation

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,37 +10,42 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
+	char *str;
 	int i = 0;
+	int flag;
 
-	va_start(args, format);
-	while (format[i] != '\0')
+	while (format)
 	{
-		if (i > 0)
-			printf(", ");
-		switch (format[i])
+		va_start(args, format);
+		while (format[i])
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
+			flag = 1;
+			switch (format[i])
 			{
-				char *str = va_arg(args, char *);
-
-				if (str == NULL)
-					printf("(nil)");
-				else
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+				case 's':
+					str = va_arg(args, char *);
+					if (!str)
+						str = "(nil)"
 					printf("%s", str);
+					break;
+				default:
+					flag = 0;
+					break;
 			}
-			break;
+			if (format[i + 1] && flag)
+				printf(", ");
+			i++;
 		}
-		i++;
+		va_end(args);
+		break;
 	}
 	printf("\n");
-	va_end(args);
 }
