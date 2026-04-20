a2a70f66100d743fcc3d2096b130c6e99e9bec14
wahabxvii <auwa.mm@gmail.com>
2026-04-20 16:10:22 +0300

    Create file 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+* print_list - prints all the elements of a list_t list.
+* @h: pointer to struct list.
+*
+* Return: number of nodes.
+*/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h != NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
