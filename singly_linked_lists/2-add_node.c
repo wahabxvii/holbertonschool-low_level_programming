7bce1ca790d8fbdbd776e67a362a6e716bcbc7cd
wahabxvii <auwa.mm@gmail.com>
2026-04-21 13:40:17 +0300

    use _strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stddef.h>
+int _strlen(char *s);
 /**
 * add_node - adds a new node at the beginning of a list_t list.
 * @head: duble pointer to list_t
@@ -19,8 +21,24 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	new_node->len = _strlen((char *)str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
+}
+/**
+ * _strlen - print the length of string
+ * @s: variable pointer
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
 }
