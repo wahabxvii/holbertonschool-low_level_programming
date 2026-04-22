eed717f53fd0eec57377bf74fb79551147ad29bc
wahabxvii <auwa.mm@gmail.com>
2026-04-22 19:35:44 +0300

    fix link with prev

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -15,8 +15,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = *head;
 	new_node->prev = NULL;
+	new_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_node;
 	*head = new_node;
 	return (new_node);
 }
