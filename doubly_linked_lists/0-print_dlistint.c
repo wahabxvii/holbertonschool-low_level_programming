825a8a6fef27f6a5f3108bbdbee616417a47efa1
wahabxvii <auwa.mm@gmail.com>
2026-04-22 13:55:46 +0300

    Create file 0-print_dlistint.c

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+* print_dlistint - prints all the elements of a dlistint_t list.
+* @h: pointer to struct
+*
+* Return: number of nodes.
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] \n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
