f568b75a3fbf26c18fdce15949dffe369e46bc73
wahabxvii <auwa.mm@gmail.com>
2026-04-23 11:19:27 +0300

    Betty style

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,6 +11,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *current = head;
 	size_t count = 0;
+
 	while (current && count < index)
 	{
 		current = current->next;
