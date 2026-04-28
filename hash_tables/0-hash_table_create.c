7ab0f8d999b292fd6d207bee692c004627ee46b2
wahabxvii <auwa.mm@gmail.com>
2026-04-28 18:21:55 +0300

    change the size of the array in new_table to hash_node_t

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!new_table)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = malloc(size * sizeof(hash_table_t *));
+	new_table->array = malloc(size * sizeof(hash_node_t *));
 	if (!new_table->array)
 	{
 		free(new_table);
