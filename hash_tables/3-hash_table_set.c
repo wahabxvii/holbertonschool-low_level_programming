51bd621741ba9992027491a937a886a163bf3be9
wahabxvii <auwa.mm@gmail.com>
2026-04-29 11:24:07 +0300

    fix collision cases

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -2,10 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 /**
-	*
-	*/
+* hash_table_set - adds an element to the hash table.
+* @ht: the hash table
+* @key: key of the node
+* @value: value of the node
+*
+* Return: 1 if success
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *prev;
+	hash_node_t *current;
 	hash_node_t *new_node;
 	unsigned long int index;
 
@@ -28,6 +35,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return(0);
 	}
 	index = key_index((const unsigned char *)key,ht->size);
+	prev = NULL;
+	current = ht->array[index];
+	while (!current)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(new_node->key);
+			free(new_node->value);
+			free(current->value);
+			current->value = strdup(value);
+			if (!current->value)
+			{
+				free(new_node);
+				return (0);
+			}
+			free(new_node);
+			return (1);
+		}
+		prev = current;
+		current = current->next;
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
