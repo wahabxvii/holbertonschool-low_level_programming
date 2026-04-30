07922e34f5ba9a1596302fee4c0647adf0835e32
wahabxvii <auwa.mm@gmail.com>
2026-04-30 11:00:19 +0300

    fix diffrent implementation to fix printing comma

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,36 +1,34 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
-* hash_table_print - prints a hash table.
-* @ht: the hash table
-*
-* Return: nothing if failed.
-*/
+ * hash_table_print - prints a hash table.
+ * @ht: the hash table
+ *
+ * Return: nothing
+ */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *node;
-	unsigned int i;
-	unsigned char comma;
+    hash_node_t *node;
+    unsigned int i;
+    unsigned char printed = 0;
 
-	if (ht == NULL)
-		return;
-	printf("{");
-	for (i = 0; i < ht->size; i++)
-	{
-		if (ht->array[i] != NULL)
-		{
-			if (comma != 1)
-				printf(", ");
-			node = ht->array[i];
-			while (node != NULL)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-				node = node->next;
-				if (node != NULL)
-					printf(", ");
-			}
-			comma = 1;
-		}
-	}
-	printf("}\n");
+    if (ht == NULL)
+        return;
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node != NULL)
+        {
+            if (printed == 1)
+                printf(", ");
+            printf("'%s': '%s'", node->key, node->value);
+            printed = 1;
+            node = node->next;
+        }
+    }
+    printf("}");
 }
+
