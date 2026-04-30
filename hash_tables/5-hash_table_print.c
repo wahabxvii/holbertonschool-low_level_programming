8e28c3160b299e48e9405c23cacefa8f3a1195d3
wahabxvii <auwa.mm@gmail.com>
2026-04-30 11:04:22 +0300

    fix Betty style

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,7 +1,6 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * hash_table_print - prints a hash table.
  * @ht: the hash table
@@ -10,25 +9,24 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *node;
-    unsigned int i;
-    unsigned char printed = 0;
+	hash_node_t *node;
+	unsigned int i;
+	unsigned char printed = 0;
 
-    if (ht == NULL)
-        return;
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            if (printed == 1)
-                printf(", ");
-            printf("'%s': '%s'", node->key, node->value);
-            printed = 1;
-            node = node->next;
-        }
-    }
-    printf("}\n");
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (printed == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			printed = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
 }
-
